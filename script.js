7127f895f28ca1b3d795b325d271ec54d27a4fea
TALUPULAPADMAPAVANI <234g1a33b3@srit.ac.in>
2025-09-13 16:04:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
-//script.js
-// Fetch a random joke from the API
-async function getJoke() {
-  try {
-    const response = await fetch('https://official-joke-api.appspot.com/random_joke');
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Fetching joke failed:", error);
-    return null;
-  }
-}
+console.log("===Hour 1:JSON Basics ===");
 
-// Update the DOM with the joke
-function displayJoke(joke) {
-  const setupEl = document.getElementById('setup');
-  const punchlineEl = document.getElementById('punchline');
-  const errorEl = document.getElementById('error');
+ let student={
+    name:"Asha",
+    age:21,
+    marks:[85,90,78]
+ }
 
-  if (!joke) {
-    setupEl.textContent = '';
-    punchlineEl.textContent = '';
-    errorEl.textContent = "Couldn't fetch a joke, try again!";
-  } else {
-    setupEl.textContent = joke.setup;
-    punchlineEl.textContent = joke.punchline;
-    errorEl.textContent = '';
-  }
-}
+ //convert object+json string
+ let jsonString=JSON.stringify(student);
+console.log("JSON string:",jsonString);
 
-// Load and display the joke
-async function loadJoke() {
-  const joke = await getJoke();
-  displayJoke(joke);
-}
+//convert JSON string+object
+ let parseObj=JSON.parse(student);
+console.log("Parsed Object:",parseObj);
+
+//fetch sample JSON from API
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+   .then(response => response.json()) //convert to json
+   .then(data => console.log("Fetched Data:",data))
+   .catch(error => console.error("Error:",error));
+
+
+
+//fetch all the users and show in console+page
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(users => {
+        let output="<h3>User List</h3><ul>";
+        users.forEach(user =>{
+            output+=`<li>${user.name} - ${user.email}</li>`;
+        });
+        output +="</ul>";
+        //Append result to page
+        document.body.innerHTML += output;
+    });
